df73e27252e022a3bcf1a782da47ea0443eaea9b
migüelito <mserranomartin+git@pm.me>
2023-01-16 19:53:50 +0100

    working carousel

--- goodreads.js
+++ goodreads.js
@@ -11,12 +11,19 @@ const layout = {
     plot_bgcolor: 'rgba(0,0,0,0)',
     modebar: {
         activecolor: 'rgb(0,0,0,0)'
-    }
-}
+    },
+    autosize: false,
+    width: WIDTH,
+    margin: {
+        l: 20,
+        r: 20,
+        b: 50,
+        t: 50,
+    },
+};
 
 function wrapped(df){
-   /*  df['ISBN'] = df['ISBN'].str.replace(/[="]+/g, '');       // ISBNs appear like ="9788418052071"
-    df['ISBN13'] = df['ISBN13'].str.replace(/[="]+/g, ''); */
+
     df.addColumn('Read This Year', df['Date Read'].dt.year().eq(2022), {inplace: true});
     df.addColumn('Added This Year', df['Date Added'].dt.year().eq(2022), {inplace: true});
     
@@ -34,9 +41,9 @@ function numberBooks(df){
     let n = df.groupby(['Read This Year']).getGroup([true])['Title'].count();
     let div = document.getElementById('number_books');
     if (n > 0){
-        div.innerHTML = "<h1 class='highlight'>"+n+"</h1>books!";
+        div.innerHTML = "<h1 class='highlight'>"+n+"</h1><h4>books!</h4>";
     } else {
-        div.innerHTML = "<h1 class='highlight'>"+0+"</h1>books :(";
+        div.innerHTML = "<h1 class='highlight'>"+0+"</h1><h4>books :(</h4>";
     }
 }
 
@@ -50,10 +57,6 @@ function countries(df) {
     
         return languages
     }
-
-}
-
-function wikidataAPI(x) {
 
 }
 
@@ -97,7 +100,7 @@ function languages(df){
             automargin: true,
             }];
         
-        Plotly.newPlot('languages', data, layout, {responsive: true});
+        Plotly.newPlot('languages', data, layout, {displayModeBar: false});
     });
 }
 
@@ -127,6 +130,9 @@ function ratingComparison(df) {
     let readThisYear = df.groupby(['Read This Year']).getGroup([true]);
     let myRating = readThisYear['My Rating'].values;
     let avgRating = readThisYear['Average Rating'].values;
+    
+    let corr = pcorr(myRating, avgRating).toFixed(2);
+    let color = gradientValue(corr);
 
     // scatterPlot
     var data = {
@@ -140,6 +146,15 @@ function ratingComparison(df) {
     };
     
     Object.assign(layout, {
+        annotations: [{
+              x: 4.5,
+              y: 5.5,
+              xref: 'x',
+              yref: 'y',
+              text: "Correlation: " + corr,
+              ax: 0,
+              ay: 0
+            }],
         xaxis: {
             automargin: true,
             title: {
@@ -154,9 +169,10 @@ function ratingComparison(df) {
         },
         yaxis: {
             automargin: true,
+            margin: 5,
             title: {
                 text: "Your rating",
-                standoff: 40
+                standoff: 10
             },
             showgrid: false,
             showline: true,
@@ -164,12 +180,18 @@ function ratingComparison(df) {
             tickmode: 'array',
             tickvals: [0,1,2,3,4,5]
         },
+        height: WIDTH
     });
-    Plotly.newPlot('rating_comp', [data], layout, {responsive: true});
+    Plotly.newPlot('rating_comp', [data], layout, {displayModeBar: false});
 
-    let corr = pcorr(myRating, avgRating).toFixed(3);
-    color = gradientValue(corr);
-    document.querySelector('#correlation').innerHTML = "There is a correlation of <h1 style='color:rgb(" + color.toString() + ")'>"+corr+"</h1> between your ratings and the average"
+    let text = document.getElementById('correlation_text');
+    if (corr < 0.5) {
+        text.innerText = "...and your opinions didn't really match with the average"
+    } else if (0.5 <=corr <0.8) {
+        text.innerText = "...and you did not disagree much with others"
+    } else {
+        text.innerText = '...and thought that the reviews were quite right!'
+    }
 }
 
 function mostRead(df){
@@ -181,8 +203,9 @@ function mostRead(df){
     div = document.getElementById('most_read');
 
     if (n>1) {
-        div.innerHTML = "Your most-read author was <h2 class='highlight'>"+mostAuthor.values+"</h2>";
+        div.innerHTML = "<h2 class='highlight'>"+mostAuthor.values+"</h2>";
     } else {
+        div.parentElement.removeChid(document.getElementById('most_read_text'));
         div.parentElement.removeChid(div);
     }
 
@@ -205,13 +228,12 @@ function monthly(df){
     
     // barplot
     Object.assign(layout, {
-        title: {
-            text: 'Books per month'
+        yaxis: {
+            showgrid: false,
+            showline: true,
         },
-        paper_bgcolor: 'rgba(0,0,0,0)',
-        plot_bgcolor: 'rgba(0,0,0,0)'
-    });
-
+        height: WIDTH,
+    })
     let data = {
         x: MONTH_NAME,
         y: y,
@@ -222,7 +244,7 @@ function monthly(df){
           opacity: 0.7,
         }
       };
-    Plotly.newPlot('monthly_reads', [data], layout, {responsive: true});
+    Plotly.newPlot('monthly_reads', [data], layout, {displayModeBar: false});
 }
 
 function newauthors(df){
@@ -233,9 +255,9 @@ function newauthors(df){
     let newNames = authorsThisYear.filter(name => !authorsPreviousYears.includes(name));
     let div = document.getElementById('new_authors');
     if (newNames.length > 0){
-        div.innerHTML = "<h1 class='highlight'>"+newNames.length+"</h1>new authors!";
+        div.innerHTML = "<h1 class='highlight'>"+newNames.length+"</h1><h4>new authors!</h4>";
     } else {
-        div.innerHTML = "<h1 class='highlight'>"+0+"</h1>new authors!";
+        div.innerHTML = "<h1 class='highlight'>"+0+"</h1><h4>new authors :(</h4>";
     }
 }
 
@@ -244,17 +266,26 @@ function readFromToReadPile(df){
     let addedBefore = df.groupby(['Added This Year']).getGroup([false])
                         .groupby(['Read This Year']).col(['Read This Year']).count();
 
-    // plot
-    Object.assign(layout, {
-        title: {
-            text: "Last year's to-read pile"
-        },
-        });
+    let stillUnread, actuallyRead;
+    if (addedBefore['Read This Year'].values.length == 2) {
+        stillUnread = addedBefore['Read This Year_count'].values[0],
+        actuallyRead = addedBefore['Read This Year_count'].values[1];
+    } else {
+        if (addedBefore['Read This Year'].values[0] == false) {
+            stillUnread = addedBefore['Read This Year_count'].values[0];
+            actuallyRead = 0;
+        } else {
+            actuallyRead = addedBefore['Read This Year_count'].values[0];
+            stillUnread = 0;
+        }
+    }
 
+    console.log()
     
+    // plot
     var data = [{
-        values: addedBefore['Read This Year_count'].values,
-        labels: ['Still unread', 'Read this year'],
+        values: [actuallyRead, stillUnread],
+        labels: ['Read this year','Still unread'],
         marker:{
             colors: PALETTE,
         },
@@ -263,26 +294,49 @@ function readFromToReadPile(df){
         automargin: true,
         }];
     
-    Plotly.newPlot('read_from_to_read', data, layout, {responsive: true});
+    Object.assign(layout, {
+        height: screen.availHeight/3.5,
+    })
     
+    let text = document.getElementById('to_read_text1')
+    
+    if (stillUnread == 0) {
+        text.innerText = "You didn't have anything on your to-read pile"
+    } else {
+        if (actuallyRead == 0){
+            text.innerText = "You didn't touch your last year's to-read pile"
+        } else if (actuallyRead/(stillUnread + actuallyRead) < 0.5) {
+            text.innerText = "Your last year's pile to-read pile is still to be read "
+        } else {
+            text.innerText = 'Wow, you are actually reading from your to-read pile'
+        }
+    }
+    Plotly.newPlot('read_from_to_read', data, layout, {displayModeBar: false}); 
 }
 
 function currentToReadPile(df){
     // #(to-read this year) vs #(to-read remaining from previous years)
     let toReadBalance = df.query(df['Exclusive Shelf'].eq('to-read'))
                             .groupby(["Added This Year"]).col(['Added This Year']).count();
-
-
-    Object.assign(layout, {
-        title: {
-            text: "Current to-read pile"
-        },
-    });
-
+    
+    let addedBefore, addedThisYear;
+    if (toReadBalance['Added This Year'].values.length == 2) {
+        addedThisYear = toReadBalance['Added This Year_count'].values[0],
+        addedBefore = toReadBalance['Added This Year_count'].values[1];
+    } else {
+        if (toReadBalance['Added This Year'].values[0] == false) {
+            addedBefore = toReadBalance['Added This Year_count'].values[0];
+            addedThisYear = 0;
+        } else {
+            addedThisYear = toReadBalance['Added This Year_count'].values[0];
+            addedBefore = 0;
+        }
+    }
+    
     // plot
     var data = [{
-        values: toReadBalance['Added This Year_count'].values,
-        labels: ['Added this year', 'Added before'],
+        values: [addedThisYear, addedBefore],
+        labels: ['Added this year', 'Added in previous years'],
         marker:{
             colors: PALETTE,
         },
@@ -291,8 +345,30 @@ function currentToReadPile(df){
         automargin: true,
         }];
     
-    Plotly.newPlot('current_to_read', data, layout, {responsive: true});
+    Object.assign(layout, {
+        height: screen.availHeight/3.5,
+    })
 
+    let text = document.getElementById('to_read_text2')
+    
+    if (addedBefore + addedThisYear == 0) {
+        text.innerText = "and you kept everything tidy, you have no books on your to-read pile!";
+    } else {
+        if (addedBefore == 0) {
+            text.innerText = 'but starting from 0, the only way is up!';
+        } else {
+            if (addedThisYear == 0){
+                text.innerText = "and you left it as it was";
+            } else if (addedThisYear/(addedBefore + addedThisYear) < 0.5) {
+                console.log(addedThisYear);
+                console.log(addedThisYear/(addedBefore + addedThisYear));
+                text.innerText = "and you have some new stuff on your plate"
+            } else {
+                text.innerText = 'and you want to make the problem worse';
+            }   
+        }
+        Plotly.newPlot('current_to_read', data, layout, {displayModeBar: false});
+    }
 }
 
 // HELPER FUNCTIONS
